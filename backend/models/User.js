dea61bd92db4e5a6ec585b0a2e944bd75c553460
biswajitdikun <bisusatapathy2000@gmail.com>
2025-03-31 12:23:16 -0400

    Login Page Update

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,7 +19,13 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    minlength: 6
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*(),.?":{}|<>])[A-Za-z\d!@#$%^&*(),.?":{}|<>]{8,}$/.test(v);
+      },
+      message: props => 'Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one number, and one special character'
+    }
   }
 }, {
   timestamps: true
